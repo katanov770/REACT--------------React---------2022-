455ca3ae9af33ca444ceb621765ea3d2152572d9
Yekutiel Katanov <katanov770@gmail.com>
2022-08-12 17:04:12 +0300

    12.08.2022

---
+++ react-course-app/src/components/CostItem.js
@@ -0,0 +1,13 @@
+function CostItem() {
+  return (
+    <div>
+      <div>Marth 2021 12</div>
+      <div>
+        <h2>Refrigerator</h2>
+        <div>$999.99</div>
+      </div>
+    </div>
+  );
+}
+
+export default CostItem;
