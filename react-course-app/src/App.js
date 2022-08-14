bf894f7bd144abe68fc2cfbe4aa8a1b7414ab54c
Yekutiel Katanov <katanov770@gmail.com>
2022-08-14 22:33:22 +0300

    14.08.2022

--- react-course-app/src/App.js
+++ react-course-app/src/App.js
@@ -1,10 +1,27 @@
-import CostItem from "./components/CostItem";
+import Costs from "./components/Costs/Costs";
 
-function App() {
+const App = () => {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: "Refrigerator",
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: "MakBook",
+      amount: 1254.72,
+    },
+    {
+      date: new Date(2021, 4, 1),
+      description: "Jeense",
+      amount: 49.99,
+    },
+  ];
   return (
     <div>
       <h1>Let's start studying React!</h1>
-      <CostItem></CostItem>
+      <Costs costs={costs} />
     </div>
   );
 }
